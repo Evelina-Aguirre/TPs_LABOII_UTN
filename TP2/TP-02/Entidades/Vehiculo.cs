df53d73782f70877ef74947cc91dac04926cd4d9
Eve <evelina.aguirre16@gmail.com>
2021-05-08 20:58:58 -0300

    Arreglos en entidad 'Vehículo'

--- TP2/TP-02/Entidades/Vehiculo.cs
+++ TP2/TP-02/Entidades/Vehiculo.cs
@@ -9,44 +9,54 @@ namespace Entidades
     /// <summary>
     /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
     /// </summary>
-    public sealed class Vehiculo
+    public abstract class Vehiculo
     {
-        enum EMarca
+        #region "Enumerados"
+        public enum EMarca
         {
             Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
         }
-        enum ETamanio
+        public enum ETamanio
         {
             Chico, Mediano, Grande
         }
+        #endregion
+
+        #region "Atributos"
         EMarca marca;
         string chasis;
         ConsoleColor color;
+        #endregion
 
+        #region "Propiedades"
         /// <summary>
         /// ReadOnly: Retornará el tamaño
         /// </summary>
-        abstract ETamanio Tamanio { get; set; }
+        public abstract ETamanio Tamanio { get;}
+        #endregion
 
+        #region"Métodos"
         /// <summary>
         /// Publica todos los datos del Vehiculo.
         /// </summary>
         /// <returns></returns>
-        sealed string Mostrar()
+        public virtual string Mostrar()
         {
-            return this;
+            return (string)this;
         }
+        #endregion
 
-        private static explicit operator string(Vehiculo p)
+        #region "Operadores"
+        public static explicit operator string(Vehiculo p)
         {
             StringBuilder sb = new StringBuilder();
 
-            sb.AppendLine("CHASIS: {0}\r\n", p.chasis);
-            sb.AppendLine("MARCA : {0}\r\n", p.marca.ToString());
-            sb.AppendLine("COLOR : {0}\r\n", p.color.ToString());
+            sb.AppendLine($"CHASIS:  {p.chasis}\r\n");
+            sb.AppendLine($"MARCA :  {p.marca.ToString()}\r\n");
+            sb.AppendLine($"COLOR :  {p.color.ToString()}\r\n");
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
 
         /// <summary>
@@ -67,7 +77,8 @@ namespace Entidades
         /// <returns></returns>
         public static bool operator !=(Vehiculo v1, Vehiculo v2)
         {
-            return (v1.chasis == v2.chasis);
+            return !(v1.chasis == v2.chasis);
         }
+        #endregion
     }
 }
